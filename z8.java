789791e776975e008db610c7db12f391b5cb1c86
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-06 15:44:06 +0400

    ЗАДАЧА 8

--- z8.java
+++ z8.java
@@ -5,21 +5,30 @@ public class z8 {
         int i=0;
         int max = m[i];
         int SumPologit = 0;
+        double SumOtric = 0.0;
         int SumChetOtric = 0;
         int KolvoPol = 0;
-        double KolvoOtric = 0;
-        Double SredArifmetOtric = 0;
+        int KolvoOtric = 0;
+        double SredArifmetOtric = 0.0;
         for (;i<14;i++) {
-           if (m[i+1] >= m[i]) max=m[i+1];
-           if (m[i] > 0) {
-               KolvoPol++;SumPologit+=m[i];
+            if (max < m[i]) max = m[i];
+            if (m[i] > 0) {
+               KolvoPol++;SumPologit += m[i];
            }
            if (m[i]<0) {
-               KolvoOtric++;Double SredArifmetOtric = ( SredArifmetOtric + (double)m[i])/KolvoOtric;
+               KolvoOtric++; SumOtric += m[i];
+               if ((m[i] % 2) == 0) SumChetOtric += m[i];
            }
-        }
 
-        System.out.println (max);
+
+
+        }
+        SredArifmetOtric =  SumOtric / KolvoOtric;
+        System.out.println ("максимальное значение "+max);
+        System.out.println ("сумма положительных элементов "+SumPologit);
+        System.out.println ("сумма четных отрицательных элементов "+SumChetOtric);
+        System.out.println ("количество положительных элементов "+KolvoPol);
+        System.out.println ("среднее арифметическое отрицательных элементов "+SredArifmetOtric);
     }
 
 }
