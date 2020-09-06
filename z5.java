4a6eb3237ebe53a45f2f5157112cc1076d00d71a
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-06 13:37:28 +0400

    ЗАДАЧА 5

--- z5.java
+++ z5.java
@@ -2,11 +2,13 @@ import java.util.*;
 public class z5 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("¬ведите целое число");
+        System.out.println("¬ведите три целых числа через пробел");
         int a = s.nextInt();
-        if (a > 0)  ++a;
-        if (a < 0)  a-=2;
-        if (a==0) a = 10;
-        System.out.println(a);
+        int b = s.nextInt();
+        int c = s.nextInt();
+        if ((a <= b) && (a <= c )) System.out.println(a);
+        if ((b <= a) && (b <= c )) System.out.println(b);
+        if ((c <= a) && (c <= b )) System.out.println(c);
+
     }
 }
