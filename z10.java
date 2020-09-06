fbf86bc260256774d6d8997d7f1ba40a58da9da7
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-06 16:56:25 +0400

    ЗАДАЧА 9 c сохранением нового массива

--- z10.java
+++ z10.java
@@ -1,13 +1,20 @@
 import java.util.*;
-public class z9 {
+public class z10 {
     public static void main(String[] args) {
         int[] m = {15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52};
-        int i=13;
+        int KolNul = 0;
 
-        for (i=13;i>=0;i--) {
-            System.out.printf (m[i]+",");
-
+        for (int i=0;i<14;i++) {
+            if (m[i] == 0) {
+                KolNul++;
+                continue;
+            }
+            System.out.printf(m[i] + ",");
         }
+            for (int i=1;i<=KolNul;i++) {
+                System.out.printf (0+",");
+            }
+
     }
 
 }
