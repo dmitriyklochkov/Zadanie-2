7e32d469ebf20804ae358174a9561be752b1118c
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-06 15:52:35 +0400

    ЗАДАЧА 9

---
+++ z9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class z9 {
+    public static void main(String[] args) {
+        int[] m = {15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52};
+        int i=13;
+
+        for (i=13;i>=0;i--) {
+            System.out.printf (m[i]+",");
+
+        }
+    }
+
+}
