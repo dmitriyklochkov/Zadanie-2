c608046aff651a6734565f8da993b7bb8f567f96
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-06 15:58:14 +0400

    ЗАДАЧА 9

--- z10.java
+++ z10.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class z9 {
     public static void main(String[] args) {
-        int[] m = {15,10,51,-6,-5,3,-10,-34,0,32,56,-12,24,-52};
+        int[] m = {15,10,0,-6,-5,3,0,-34,0,32,56,0,24,-52};
+        int i=13;
 
-        for (int i=13;i>=0;i--) {
+        for (i=13;i>=0;i--) {
             System.out.printf (m[i]+",");
 
         }
