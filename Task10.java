28f0c55e78279b1fee9b72037af2f246b3629d4a
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-10 21:30:05 +0400

    Исправил 10

--- Task10.java
+++ Task10.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class Task10 {
     public static void main(String[] args) {
-        int[] m = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
-        int[] n = new int[14];
+        int[] array1 = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
+        int[] array2 = new int[array1.length];
         int quantityNul = 0;
         int k = 0;
-        for (int i = 0; i < 14; i++) {
-            if (m[i] != 0) {
-                n[k] = m[i];
+        for (int i = 0; i < array1.length; i++) {
+            if (array1[i] != 0) {
+                array2[k] = array1[i];
                 k++;
             } else {
                 quantityNul++;
                 continue;
             }
         }
-        k = 13;
+        k = array1.length-1;
         for (int i = 1; i <= quantityNul; i++) {
-            n[k] = 0;
+            array2[k] = 0;
             k--;
         }
 
-        String nString = Arrays.toString(n);
+        String nString = Arrays.toString(array2);
         System.out.println(nString);
     }
 
